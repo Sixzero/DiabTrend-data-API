f35757f7a01d1a700a7268ec488c0fe064b9718c
SixZero <havliktomi@hotmail.com>
2022-12-07 14:11:23 +0100

    Minor improvements.

--- example.py
+++ example.py
@@ -8,13 +8,17 @@ response_login = requests.post(
         "password": "example1234",
         "returnSecureToken": "true"
     })
-id_token = response_login.json()['idToken']
+result = response_login.json()
+id_token = result['idToken']
 
 data = {
-    "data": {"all": "false", "date": "2021-12-31T23:00:00.000Z", "endData": "2022-12-31T23:00:00.000Z", "type": "csv"}}
+    "data": {
+        "all": "false", "date": "2021-12-31T23:00:00.000Z", "endData": "2022-12-31T23:00:00.000Z", "type": "csv",
+    }
+}
 
-response_export = requests.post('https://us-central1-diabtrend-db.cloudfunctions.net/exportToCsv', json=data,
-                          headers={'authorization': 'Bearer ' + id_token})
+response_export = requests.post('https://us-central1-diabtrend-db.cloudfunctions.net/exportToCsv_open',
+                                json=data, headers={'authorization': 'Bearer ' + id_token})
 
 data_in_bytes = str.encode(response_export.json()['result']['base64'])
 with open("out.csv", "wb") as fh:
